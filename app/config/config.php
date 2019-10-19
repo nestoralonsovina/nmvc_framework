2a0a92c6c985e8bcbdcda1f50e19a26b18fb1dd7
Néstor Alonso Viña <alonsonestor20@gmail.com>
2019-10-19 18:01:07 +0200

    Basic Framework with Example

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', 'root');
+  define('DB_NAME', 'nmvc');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', $_SERVER['HTTP_HOST']);
+  // Site Name
+  define('SITENAME', 'NalonsoMVC');
