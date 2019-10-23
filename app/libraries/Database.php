2330ebfed45411c988c0781fb675bc418fa08a6b
Néstor Alonso Viña <alonsonestor20@gmail.com>
2019-10-23 14:59:14 +0200

    Fixed small mistake

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -14,7 +14,7 @@
  * Params in /app/config/config.php
  * Class Database
  */
-abstract class Database {
+class Database {
     private $host = DB_HOST;
     private $user = DB_USER;
     private $pass = DB_PASS;
