7248a37cb20e5915d680ab99bfe56f6ad31c3a3a
Néstor Alonso Viña <alonsonestor20@gmail.com>
2019-10-23 11:22:31 +0200

    Documented the main parts of the framework

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -1,11 +1,18 @@
 <?php
-  /*
-   * Base Controller
-   * Loads the models and views
-   */
-  class Controller {
-    // Load model
-    public function model($model){
+
+/**
+ * Loads the views and controllers
+ * Class Controller
+ */
+abstract class Controller {
+      /**
+       * This function expects the name of a model to charge.
+       * The model is a class in charge of managing all the data of a
+       * specific genre..
+       * @param $model name of the model to load
+       * @return mixed model to laod
+       */
+      public function model($model){
       // Require model file
       require_once '../app/models/' . $model . '.php';
 
@@ -13,8 +20,15 @@
       return new $model();
     }
 
-    // Load view
-    public function view($view, $data = []){
+      /**
+       * The view function takes a path to a view in the views folder.
+       * You have the option to pass an array of data from the controller
+       * which can then be accessed on the .php file assigned. A good example
+       * of this behaviour is the Pages.php controller and it's corresponding view.
+       * @param $view name of the view to load
+       * @param array $data
+       */
+      public function view($view, array $data = []){
       // Check for view file
       if(file_exists('../app/views/' . $view . '.php')){
         require_once '../app/views/' . $view . '.php';
--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -1,12 +1,20 @@
 <?php
-  /*
-   * PDO Database Class
-   * Connect to database
-   * Create prepared statements
-   * Bind values
-   * Return rows and results
-   */
-  class Database {
+/*
+ * PDO Database Class
+ * Connect to database
+ * Create prepared statements
+ * Bind values
+ * Return rows and results
+ */
+
+/**
+ * In charge of connecting to the database.
+ * It should be extended by the models,
+ * shouldn't be used without them.
+ * Params in /app/config/config.php
+ * Class Database
+ */
+abstract class Database {
     private $host = DB_HOST;
     private $user = DB_USER;
     private $pass = DB_PASS;
@@ -16,68 +24,92 @@
     private $stmt;
     private $error;
 
+    /**
+     * Sets the connection to the database
+     * Database constructor.
+     */
     public function __construct(){
-      // Set DSN
-      $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
-      $options = array(
-        PDO::ATTR_PERSISTENT => true,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-      );
+        // Set DSN
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+        $options = array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
 
-      // Create PDO instance
-      try{
-        $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
-      } catch(PDOException $e){
-        $this->error = $e->getMessage();
-        echo $this->error;
-      }
-    }
-
-    // Prepare statement with query
-    public function query($sql){
-      $this->stmt = $this->dbh->prepare($sql);
-    }
-
-    // Bind values
-    public function bind($param, $value, $type = null){
-      if(is_null($type)){
-        switch(true){
-          case is_int($value):
-            $type = PDO::PARAM_INT;
-            break;
-          case is_bool($value):
-            $type = PDO::PARAM_BOOL;
-            break;
-          case is_null($value):
-            $type = PDO::PARAM_NULL;
-            break;
-          default:
-            $type = PDO::PARAM_STR;
+        // Create PDO instance
+        try {
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+        } catch(PDOException $e){
+            $this->error = $e->getMessage();
+            echo $this->error;
         }
-      }
-
-      $this->stmt->bindValue($param, $value, $type);
     }
 
-    // Execute the prepared statement
+    /**
+     * Prepares the sql statement
+     * @param $sql string query to execute
+     */
+    public function query($sql){
+        $this->stmt = $this->dbh->prepare($sql);
+    }
+
+    /**
+     * Binds the :param to it's value set before in the query method
+     * @param $param string :key
+     * @param $value string value
+     * @param null $type type of the value, if not set it'll automatically be set
+     */
+    public function bind($param, $value, $type = null){
+        if(is_null($type)){
+            switch(true){
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    /**
+     * Executes the prepared statement
+     * @return boolean true if success false if not
+     */
     public function execute(){
-      return $this->stmt->execute();
+        return $this->stmt->execute();
     }
 
-    // Get result set as array of objects
+    /**
+     * Returns all the solutions found as an array of objects
+     * @return mixed returns all the objects found with the query
+     */
     public function resultSet(){
-      $this->execute();
-      return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
     }
 
-    // Get single record as object
+    /**
+     * Returns a single row
+     * @return mixed
+     */
     public function single(){
-      $this->execute();
-      return $this->stmt->fetch(PDO::FETCH_OBJ);
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
     }
 
-    // Get row count
+    /**
+     * returns the row count
+     * @return mixed
+     */
     public function rowCount(){
-      return $this->stmt->rowCount();
+        return $this->stmt->rowCount();
     }
-  }
+}
